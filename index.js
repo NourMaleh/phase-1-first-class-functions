1f6c6921f6b82178ffa0bb80de298f42f163472c
nour al maleh <nourmale7@gmail.com>
2022-12-06 22:53:39 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function receivesAFunction(callback) {
+    return callback()}
+
+
+    function callback() {
+      
+    }
+
+
+    function returnsANamedFunction() {
+        return function whatever(){
+         
+        } }
+
+
+        function returnsAnAnonymousFunction() {
+        
+            return function(){
+
+            }}
+
+    
